cfbe6429fc26f5d4b25ec7a8b7dcc85a68b2a1f6
unknown <ekeinchiaemmanuela@gmail.com>
2022-05-17 13:13:38 -0700

    checking if year is a leap year or not

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-year = int(input("Enter the year you want to chech"))
+year = int(input("Enter the year you want to checK :"))
+leap = True
+
+if year %4 == 0:
+    if year % 100 != 0:
+        leap = True
+    elif year % 400 == 0:
+        leap = True
+    else:
+        leap = False
+else:
+    leap = False
+
+if leap == True:
+    print(f"the year {year} is a leap year")
+else:
+    print(f"the year {year} is not a leap year")
